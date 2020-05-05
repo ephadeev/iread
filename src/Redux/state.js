bced4fa4e09b5c2308db41b3a593467427a268db
ephadeev <itrex1991@hotmail.com>
2020-05-05 20:17:15 +0300

    maked some refactor

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,59 +1,68 @@
-import {renderEntireTree} from "../render";
-
-let state = {
-    users: [
-        {name: 'Richard Hendricks', image: 'https://www.kinopoisk.ru/images/sm_actor/1615667.jpg'},
-        {name: 'Nelson Bighetti', image: 'https://www.kinopoisk.ru/images/sm_actor/1852968.jpg'},
-        {name: 'Bertram Gilfoyle', image: 'https://www.kinopoisk.ru/images/sm_actor/11897.jpg'},
-        {name: 'Dinesh Chugtai', image: 'https://www.kinopoisk.ru/images/sm_actor/1833413.jpg'},
-        {name: 'Monica Hall', image: 'https://www.kinopoisk.ru/images/sm_actor/731114.jpg'},
-        {name: 'Jared', image: 'https://www.kinopoisk.ru/images/sm_actor/1085386.jpg'}
-    ],
-    posts: [
-        {id: 0, userId: 0, text: 'Hello! How are you?', isPrivate: false},
-        {id: 1, userId: 1, text: 'Im fine thank you!', isPrivate: false},
-        {id: 2, userId: 2, text: 'Where are you?', isPrivate: false},
-        {id: 3, userId: 3, text: 'Whats uuuuuuup', isPrivate: false},
-        {id: 4, userId: 4, text: 'I am going home.', isPrivate: false},
-        {id: 5, userId: 5, text: 'Here we home again', isPrivate: false},
-        {id: 6, userId: 0, text: 'love this book.', isPrivate: false},
-        {id: 7, userId: 0, text: 'nice one', isPrivate: false}
-    ],
-    careers: {
-        shortcomings: [
-            {id: 0, text: 'watched only the first part of the godfather', isChecked: false},
-            {id: 1, text: 'don\'t eat olives', isChecked: false},
-            {id: 2, text: 'don\'t like Russian films', isChecked: false},
-            {id: 3, text: 'don\'t have telekinesis', isChecked: false},
-            {id: 4, text: 'don\'t speak elven', isChecked: false},
-            {id: 5, text: 'bad sense of humor', isChecked: false},
-            {id: 6, text: 'too good sense of humor', isChecked: false}
+const store = {
+    _state: {
+        users: [
+            {name: 'Richard Hendricks', image: 'https://www.kinopoisk.ru/images/sm_actor/1615667.jpg'},
+            {name: 'Nelson Bighetti', image: 'https://www.kinopoisk.ru/images/sm_actor/1852968.jpg'},
+            {name: 'Bertram Gilfoyle', image: 'https://www.kinopoisk.ru/images/sm_actor/11897.jpg'},
+            {name: 'Dinesh Chugtai', image: 'https://www.kinopoisk.ru/images/sm_actor/1833413.jpg'},
+            {name: 'Monica Hall', image: 'https://www.kinopoisk.ru/images/sm_actor/731114.jpg'},
+            {name: 'Jared', image: 'https://www.kinopoisk.ru/images/sm_actor/1085386.jpg'}
         ],
-        checkedElements: 0
+        posts: [
+            {id: 0, userId: 0, text: 'Hello! How are you?', isPrivate: false},
+            {id: 1, userId: 1, text: 'Im fine thank you!', isPrivate: false},
+            {id: 2, userId: 2, text: 'Where are you?', isPrivate: false},
+            {id: 3, userId: 3, text: 'Whats uuuuuuup', isPrivate: false},
+            {id: 4, userId: 4, text: 'I am going home.', isPrivate: false},
+            {id: 5, userId: 5, text: 'Here we home again', isPrivate: false},
+            {id: 6, userId: 0, text: 'love this book.', isPrivate: false},
+            {id: 7, userId: 0, text: 'nice one', isPrivate: false}
+        ],
+        careers: {
+            shortcomings: [
+                {id: 0, text: 'watched only the first part of the godfather', isChecked: false},
+                {id: 1, text: 'don\'t eat olives', isChecked: false},
+                {id: 2, text: 'don\'t like Russian films', isChecked: false},
+                {id: 3, text: 'don\'t have telekinesis', isChecked: false},
+                {id: 4, text: 'don\'t speak elven', isChecked: false},
+                {id: 5, text: 'bad sense of humor', isChecked: false},
+                {id: 6, text: 'too good sense of humor', isChecked: false}
+            ],
+            checkedElements: 0
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    setState(value) {
+        this._state = value;
+    },
+    _renderEntireTree() {
+        console.log('state changed');
+    },
+    checkAmountOfCheckedElements(status, id) {
+        if (status) {
+            this._state.careers.checkedElements++;
+            this._state.careers.shortcomings[id].isChecked = true;
+        } else {
+            this._state.careers.checkedElements--;
+            this._state.careers.shortcomings[id].isChecked = false;
+        }
+        this._renderEntireTree(this.getState());
+    },
+    addPost(text) {
+        let post = {
+            id: 8,
+            userId: 0,
+            text: text,
+            isPrivate: false
+        };
+        this._state.posts.push(post);
+        this._renderEntireTree(this.getState());
+    },
+    subscribe(observer) {
+        this._renderEntireTree = observer;
     }
 };
 
-export let checkAmountOfCheckedElements = (status, id) => {
-    if (status) {
-        state.careers.checkedElements++;
-        state.careers.shortcomings[id].isChecked = true;
-    } else {
-        state.careers.checkedElements--;
-        state.careers.shortcomings[id].isChecked = false;
-    }
-    console.log(state.careers.checkedElements);
-    renderEntireTree(state);
-};
-
-export let addPost = (text) => {
-    let post = {
-        id: 8,
-        userId: 0,
-        text: text,
-        isPrivate: false
-    };
-    state.posts.push(post);
-    renderEntireTree(state);
-};
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import * as serviceWorker from './serviceWorker';
-import {renderEntireTree} from './render';
-import state from "./Redux/state";
+import './all.min.css';
+import './index.css';
+import store from "./Redux/state";
+import App from "./App";
 
-renderEntireTree(state);
+
+const renderEntireTree = (state) => {
+    ReactDOM.render(
+        <App state={state}
+             checkAmountOfCheckedElements={store.checkAmountOfCheckedElements.bind(store)}
+             addPost={store.addPost.bind(store)} />,
+        document.getElementById('root')
+    );
+};
+
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
