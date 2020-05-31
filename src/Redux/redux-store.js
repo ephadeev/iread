4d45e87e960924674224d245a2b279dca9bd42a1
ephadeev <itrex1991@hotmail.com>
2020-05-31 22:33:16 +0300

    added redux-logger

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -2,14 +2,17 @@ import {combineReducers, createStore, applyMiddleware} from "redux";
 import activityReducer from "./reducers/activity-reducer";
 import careersReducer from "./reducers/careers-reducer";
 import usersReducer from "./reducers/users-reducer";
+import firebaseReducer from "./reducers/firebase-reducer";
 import thunk from 'redux-thunk';
+import reduxLogger from 'redux-logger';
 
 let reducers = combineReducers({
     activity: activityReducer,
     careers: careersReducer,
-    users: usersReducer
+    users: usersReducer,
+    firebase: firebaseReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunk));
+let store = createStore(reducers, applyMiddleware(thunk, reduxLogger));
 
 export default store;
