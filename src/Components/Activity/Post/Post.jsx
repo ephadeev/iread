54867c429987f1063faf97ee723742c5d8846c4e
ephadeev <itrex1991@hotmail.com>
2020-06-17 21:11:13 +0300

    fixed styles

--- src/Components/Activity/Post/Post.jsx
+++ src/Components/Activity/Post/Post.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import stylesPost from "./Post.module.css";
+import '../../../App.css';
 
 const Post = ({userId, postText}) => {
     const getAuthorOfPostData = (userId) => {
@@ -18,7 +19,7 @@ const Post = ({userId, postText}) => {
         <div className={stylesPost.posts}>
             <div className={stylesPost.container}>
                 <span>
-                    <img className={stylesPost.posts__image}
+                    <img className='small-avatar'
                          src={authorOfPost?.image}
                          alt="Profile avatar"/>
                     <span className={stylesPost.posts__author}>
--- src/Components/Profile/Wall/Post/Post.jsx
+++ src/Components/Profile/Wall/Post/Post.jsx
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import firebase from 'firebase/app';
 import stylesPost from './Post.module.css';
-import {deletePost} from '../../../../Redux/actions/firebase-actions';
+import '../../../../App.css';
+import {deletePost} from '../../../../Redux/actions/posts-actions';
 
 const Post = ({postText, postId, deletePost}) => {
     const delPost = () => firebase.firestore().collection('posts').doc(postId).delete()
-        .then(deletePost(postId))
+        .then(() => deletePost(postId))
         .catch(err => console.log(err.message));
 
     return (
         <div className={stylesPost.posts}>
             {postText}
-            <i className={`fas fa-trash ${stylesPost.trash}`}
+            <i className='fas fa-trash buttons'
                onClick={delPost}>
             </i>
         </div>
