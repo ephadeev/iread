0eaa880a6f227a5c6cf7c1806e353b5c60e038f7
ephadeev <itrex1991@hotmail.com>
2020-07-22 00:29:40 +0300

    add messages themes and editProfile reducers

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,8 +1,10 @@
 import {combineReducers, createStore, applyMiddleware} from 'redux';
 import usersReducer from './reducers/users-reducer';
 import postsReducer from './reducers/posts-reducer';
+import messagesReducer from './reducers/messages-reducer';
 import authorizationReducer from './reducers/authorization-reducer';
 import editProfileReducer from './reducers/editProfile-reducer';
+import themesReducer from './reducers/themes-reducer';
 import thunk from 'redux-thunk';
 import reduxLogger from 'redux-logger';
 
@@ -10,7 +12,9 @@ let reducers = combineReducers({
     authorization: authorizationReducer,
     users: usersReducer,
     posts: postsReducer,
-    editProfile: editProfileReducer
+    messages: messagesReducer,
+    editProfile: editProfileReducer,
+    themes: themesReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunk, reduxLogger));
