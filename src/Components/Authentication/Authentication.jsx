709850be89c43e478e7009e1548c5c3720460687
ephadeev <itrex1991@hotmail.com>
2020-09-25 04:26:42 +0300

    add error message in authentication

--- src/Components/Authentication/Authentication.jsx
+++ src/Components/Authentication/Authentication.jsx
@@ -5,10 +5,11 @@ import '../../App.css';
 import SignIn from './Sign-in/SignIn';
 import SignUp from './Sign-up/SignUp';
 
-const Authentication = ({checkedTheme}) => {
+const Authentication = ({checkedTheme, error}) => {
     return (
         <div className={`wrapper bgColorDefault bgColor${checkedTheme}`}>
             <div className='container bgColorGray'>
+                <div className='authentication__error'>{error}</div>
                 <div className='flex-container'>
                     <SignIn />
                     <SignUp />
@@ -19,12 +20,14 @@ const Authentication = ({checkedTheme}) => {
 };
 
 Authentication.propTypes = {
-    checkedTheme: PropTypes.string
+    checkedTheme: PropTypes.string,
+    error: PropTypes.string
 };
 
 const mspStateToProps = state => {
     return {
-        checkedTheme: state.themes.checkedTheme
+        checkedTheme: state.themes.checkedTheme,
+        error: state.authorization.error
     }
 };
 
