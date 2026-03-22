6b04079ed82c132a5b4a17c3af239242f1661346
ephadeev <itrex1991@hotmail.com>
2026-03-22 21:29:59 +0300

    made memoization for Activity component

--- src/pages/ui/Activity/ActivityContainer.tsx
+++ src/pages/ui/Activity/ActivityContainer.tsx
@@ -1,9 +1,12 @@
 import Activity from "./Activity.tsx";
 import { useGetPostsQuery } from "@/entities/post/api/post.api.ts";
+import { useAppSelector } from "@/shared/store/lib/reduxHooks.ts";
+import { getCheckedTheme } from "@/shared/store/model/themeSlice.ts";
 
 const ActivityContainer = () => {
+	const checkedTheme = useAppSelector(getCheckedTheme);
 	const { data: posts } = useGetPostsQuery();
 
-	return <Activity postsFromProps={posts} />;
+	return posts && <Activity posts={posts} checkedTheme={checkedTheme} />;
 };
 export default ActivityContainer;
