888dc99ca9565c7ca1e880c57feafef82b9c89cd
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:43:12 +0300

    redesign width MUI for DeleteFriendButton component

--- src/features/DeleteFriend/ui/DeleteFriendButton.tsx
+++ src/features/DeleteFriend/ui/DeleteFriendButton.tsx
@@ -1,35 +1,34 @@
-import { FC } from "react";
-import "@/app/App.css";
-import { useAppSelector } from "@/shared/store/lib/reduxHooks.ts";
-import { getCheckedTheme } from "@/shared/store/model/themeSlice.ts";
-import { useAuthUser } from "@/entities/user/api/useAuthUser.ts";
-import { useDeleteFriendMutation } from "@/entities/user/api/user.api.ts";
+import {FC} from "react";
+import {IconButton, Tooltip} from '@mui/material';
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import {useAuthUser} from "@/entities/user/api/useAuthUser.ts";
+import {useDeleteFriendMutation} from "@/entities/user/api/user.api.ts";
 import Loader from "@/shared/ui/Loader/Loader.tsx";
 
 const DeleteFriendButton: FC<{
-	friendsId: string;
-}> = ({ friendsId }) => {
-	const checkedTheme = useAppSelector(getCheckedTheme);
-	const { uid } = useAuthUser();
-	if (!uid) {
-		throw new Error(
-			"Auth invariant violated: uid is null inside protected route",
-		);
-	}
-	const [deleteFriend, { isLoading }] = useDeleteFriendMutation();
+    friendsId: string;
+}> = ({friendsId}) => {
+    const {uid} = useAuthUser();
+    if (!uid) {
+        throw new Error(
+            "Auth invariant violated: uid is null inside protected route",
+        );
+    }
+    const [deleteFriend, {isLoading}] = useDeleteFriendMutation();
 
-	if (isLoading) return <Loader />;
+    if (isLoading) return <Loader/>;
 
-	const deleteFriendHandler = () => {
-		deleteFriend({ currentUserUid: uid, friendUid: friendsId });
-	};
+    const deleteFriendHandler = () => {
+        deleteFriend({currentUserUid: uid, friendUid: friendsId});
+    };
 
-	return (
-		<i
-			className={`fas fa-user-times buttons colorDefault color${checkedTheme}`}
-			onClick={deleteFriendHandler}
-		></i>
-	);
+    return (
+        <Tooltip title='Delete friend'>
+            <IconButton onClick={deleteFriendHandler}>
+                <PersonRemoveIcon/>
+            </IconButton>
+        </Tooltip>
+    );
 };
 
 export default DeleteFriendButton;
