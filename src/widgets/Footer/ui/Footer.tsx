515492f38499c02e1104d1deaf0381e6b5b54ae4
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:43:54 +0300

    redesign width MUI for Footer component

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,28 +1,22 @@
-import "@/app/App.css";
-import stylesFooter from "./Footer.module.css";
-import { FC } from "react";
-import { useAppSelector } from "@/shared/store/lib/reduxHooks.ts";
-import { getCheckedTheme } from "@/shared/store/model/themeSlice.ts";
+import {Box, Container, Link} from '@mui/material';
+import {FC} from "react";
 
 const Footer: FC = () => {
-	const checkedTheme = useAppSelector(getCheckedTheme);
-
-	return (
-		<footer className={`bgColorDefault bgColor${checkedTheme}`}>
-			<div
-				className={`${stylesFooter.container} container texAlCenter footer__container`}
-			>
-				<a
-					href="https://www.linkedin.com/in/evgeny-phadeev-0a639899/?locale=en_US"
-					target="_blank"
-					rel="noopener noreferrer"
-					className={`footer__link colorDefault color${checkedTheme}`}
-				>
-					developed by ephadeev
-				</a>
-			</div>
-		</footer>
-	);
+    return (
+        <Box component='footer'>
+            <Container maxWidth='xl' sx={{display: 'flex', justifyContent: 'center'}}>
+                <Link
+                    href="https://www.linkedin.com/in/evgeny-phadeev-0a639899/?locale=en_US"
+                    underline='hover'
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label='visit linkedIn profile of developer'
+                >
+                    developed by ephadeev
+                </Link>
+            </Container>
+        </Box>
+    );
 };
 
 export default Footer;
