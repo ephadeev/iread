352f57fd1807267405a1bdbe8a1501f3a017895f
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:44:11 +0300

    redesign width MUI for Friends component

--- src/pages/ui/Friends/Friends.tsx
+++ src/pages/ui/Friends/Friends.tsx
@@ -1,24 +1,23 @@
-import "@/app/App.css";
+import {Box, Container, List} from '@mui/material';
 import FriendsList from "./FriendsList.tsx";
-import { FC } from "react";
-import { useAppSelector } from "@/shared/store/lib/reduxHooks.ts";
-import { getCheckedTheme } from "@/shared/store/model/themeSlice.ts";
-import { useAuthUser } from "@/entities/user/api/useAuthUser.ts";
+import {FC} from "react";
+import {useAuthUser} from "@/entities/user/api/useAuthUser.ts";
 
 const Friends: FC = () => {
-	const checkedTheme = useAppSelector(getCheckedTheme);
-	const { friends } = useAuthUser();
+    const {friends} = useAuthUser();
 
-	return (
-		<main className={`wrapper bgColorDefault bgColor${checkedTheme}`}>
-			<div className="container bgColorGray">
-				{friends.length !== 0 &&
-					friends?.map((friendId) => (
-						<FriendsList friendId={friendId} key={friendId} />
-					))}
-			</div>
-		</main>
-	);
+    return (
+        <Box component='main' sx={{minHeight: 'calc(100vh - 120px - 10px - 24px)'}}>
+            <Container maxWidth='xl'>
+                <List>
+                    {friends.length !== 0 &&
+                        friends?.map((friendId, i) => (
+                            <FriendsList friendId={friendId} key={friendId} withDivider={i !== friends.length - 1}/>
+                        ))}
+                </List>
+            </Container>
+        </Box>
+    );
 };
 
 export default Friends;
