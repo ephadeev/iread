9d8e71cf1b5215e6173d5963c712d8ec7d48a7f4
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:46:20 +0300

    redesign width MUI for UploadImage components

--- src/features/UploadImage/ui/UploadImage.tsx
+++ src/features/UploadImage/ui/UploadImage.tsx
@@ -1,43 +1,49 @@
 import "@/app/App.css";
-import { ChangeEvent, FC } from "react";
-import { useUploadProfileImageMutation } from "@/entities/user/api/user.api.ts";
-import { useAuthUser } from "@/entities/user/api/useAuthUser.ts";
+import {ChangeEvent, FC} from "react";
+import {useUploadProfileImageMutation} from "@/entities/user/api/user.api.ts";
+import {useAuthUser} from "@/entities/user/api/useAuthUser.ts";
 
 const UploadImage: FC = () => {
-	const [uploadImage] = useUploadProfileImageMutation();
-	const { uid } = useAuthUser();
-	if (!uid) {
-		throw new Error(
-			"Auth invariant violated: uid is null inside protected route",
-		);
-	}
+    const [uploadImage] = useUploadProfileImageMutation();
+    const {uid} = useAuthUser();
+    if (!uid) {
+        throw new Error(
+            "Auth invariant violated: uid is null inside protected route",
+        );
+    }
 
-	const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-		event.preventDefault();
-		// Get file
-		const files = event.target.files;
-		if (files && files.length > 0) {
-			const file = files[0];
-			console.log("called onFileChange");
-			// TODO: need to delete previous photo
-			uploadImage({ currentUserUid: uid, file });
-		}
-	};
+    const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        // Get file
+        const files = event.target.files;
+        if (files && files.length > 0) {
+            const file = files[0];
+            console.log("called onFileChange");
+            // TODO: need to delete previous photo
+            uploadImage({currentUserUid: uid, file});
+        }
+    };
 
-	return (
-		<div className="upload-image">
-			<form>
-				<input
-					type="file"
-					id="fileButton"
-					name="file"
-					accept="image/png, .jpeg, .jpg, image/gif"
-					required
-					onChange={onFileChange}
-				/>
-			</form>
-		</div>
-	);
+    return (
+        <input type="file"
+               id="fileButton"
+               name="file"
+               accept="image/png, .jpeg, .jpg, image/gif"
+               style={{
+                   border: 0,
+                   clip: 'rect(0 0 0 0)',
+                   height: '1px',
+                   margin: '-1px',
+                   overflow: 'hidden',
+                   padding: 0,
+                   position: 'absolute',
+                   whiteSpace: 'nowrap',
+                   width: '1px',
+               }}
+               required
+               onChange={onFileChange}
+        />
+    );
 };
 
 export default UploadImage;
