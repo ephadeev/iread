cb20f73c0a535a9723785b194c0711953fe76d22
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:48:15 +0300

    redesign width MUI for App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,42 @@
-import { FC } from "react";
+import {FC} from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 import "./App.css";
-import Nav from "@/widgets/Nav/ui/Nav.tsx";
 import Footer from "@/widgets/Footer/ui/Footer.tsx";
 import Header from "@/widgets/Header/ui/Header.tsx";
 import AppRoutes from "@/pages/lib/routes.tsx";
 
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        // success: {
+        //     main: '#ff0000',
+        // },
+        background: {
+            default: '#201f20',
+            paper: '#333333'
+        }
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: (themeParam) => `
+        li {
+          background-color: ${themeParam.palette.background.paper};
+        }
+      `,
+        },
+    }
+});
+
 const App: FC = () => {
-	return (
-		<div className="App">
-			<Header />
-			<Nav />
-			<AppRoutes />
-			<Footer />
-		</div>
-	);
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Header/>
+            <AppRoutes/>
+            <Footer/>
+        </ThemeProvider>
+    );
 };
 
 export default App;
