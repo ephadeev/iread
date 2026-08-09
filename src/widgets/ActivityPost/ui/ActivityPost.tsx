10297c4a5ff458f42dd91f0dd3c82d197ec02e71
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:41:09 +0300

    redesign width MUI for ActivityPost component

--- src/widgets/ActivityPost/ui/ActivityPost.tsx
+++ src/widgets/ActivityPost/ui/ActivityPost.tsx
@@ -1,54 +1,73 @@
-import { FC } from "react";
-import stylesPost from "./ActivityPost.module.css";
+import {FC, useState} from "react";
+import Avatar from '@mui/material/Avatar';
+import {Badge, Divider, IconButton, ListItem, ListItemAvatar, ListItemText, Tooltip, Typography} from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import "@/app/App.css";
-import { useGetUserByIdQuery } from "@/entities/user/api/user.api.ts";
-import {
-	handleUserAvatarError,
-	USER_UNKNOWN_ICON_URL,
-} from "@/app/userUnknownIconUrl.ts";
-import { Link } from "react-router";
+import {useGetUserByIdQuery} from "@/entities/user/api/user.api.ts";
+import {handleUserAvatarError, USER_UNKNOWN_ICON_URL,} from "@/app/userUnknownIconUrl.ts";
+import {Link} from "react-router";
 
 const ActivityPost: FC<{
-	userId: string;
-	postText: string;
-	hours: string;
-	minutes: string;
-	day: number;
-	month: string;
-	year: number;
-	checkedTheme: string;
-}> = ({ userId, postText, hours, minutes, day, month, year, checkedTheme }) => {
-	const { data: userData } = useGetUserByIdQuery(userId as string, {
-		skip: !userId,
-	});
+    userId: string;
+    postText: string;
+    hours: string;
+    minutes: string;
+    day: number;
+    month: string;
+    year: number;
+    withDivider: boolean;
+}> = ({userId, postText, hours, minutes, day, month, year, withDivider}) => {
 
-	return (
-		<div className={stylesPost.posts}>
-			<div className="container flex-container bgColorGray post__container">
-				<>
-					<Link to={`/users/${userId}`} className="links">
-						<img
-							className="small-avatar"
-							src={userData?.image || USER_UNKNOWN_ICON_URL}
-							alt="Profile avatar"
-							onError={handleUserAvatarError}
-						/>
-					</Link>
-					<span className={stylesPost.posts__author}>
-						{`${userData?.firstName || "John"} 
-                        ${userData?.lastName || "Smith"}:`}
-					</span>
-				</>
-				<span className="post__text">{postText}</span>
-				<span
-					className={`post__time colorDefault color${checkedTheme}`}
-				>{`${hours}:${minutes}`}</span>
-				<span
-					className={`post__date colorDefault color${checkedTheme}`}
-				>{`${day}.${month}.${year}`}</span>
-			</div>
-		</div>
-	);
+    // TODO: get likesCount from firestore
+    const [likesCount, setLikesCount] = useState(0);
+    const {data: userData} = useGetUserByIdQuery(userId as string, {
+        skip: !userId,
+    });
+
+    // TODO: write user id in firestore
+    const toggleLike = () => {
+        setLikesCount(likesCount + 1);
+    }
+
+    return (
+        <>
+            <ListItem secondaryAction={
+                <Tooltip title='Like post'>
+                    <IconButton onClick={toggleLike}>
+                        <Badge badgeContent={likesCount} color='secondary'>
+                            <FavoriteIcon/>
+                        </Badge>
+                    </IconButton>
+                </Tooltip>
+            }>
+                <Link to={`/users/${userId}`} className="links">
+                    {/*TODO: on Avatar mouseover show a little bit more info about user*/}
+                    <ListItemAvatar>
+                        <Avatar
+                            src={userData?.image || USER_UNKNOWN_ICON_URL}
+                            alt='Profile avatar'
+                            sx={{width: 50, height: 50}}
+                            slotProps={{
+                                img: {onError: handleUserAvatarError},
+                            }}
+                        >{userData?.firstName[0]}{userData?.lastName[0]}</Avatar>
+                    </ListItemAvatar>
+                </Link>
+                <ListItemText
+                    primary={
+                        <Typography>
+                            {`${userData?.firstName} ${userData?.lastName}: ${postText}`}
+                        </Typography>
+                    }
+                />
+                <ListItemText
+                    secondary={`${hours}:${minutes} ${day}.${month}.${year}`}
+                    sx={{flexGrow: 0, marginRight: 2}}
+                />
+            </ListItem>
+            {withDivider && <Divider component='li' aria-hidden='true'/>}
+        </>
+    );
 };
 
 export default ActivityPost;
