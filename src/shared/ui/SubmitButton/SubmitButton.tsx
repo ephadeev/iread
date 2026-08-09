4d36dcce60fe962e41f890c4e9debf49111a904a
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:46:38 +0300

    redesign width MUI for SubmitButton components

--- src/shared/ui/SubmitButton/SubmitButton.tsx
+++ src/shared/ui/SubmitButton/SubmitButton.tsx
@@ -1,18 +1,19 @@
-import { FC } from "react";
-import { useFormStatus } from "react-dom";
-import styles from "./SubmitButton.module.css";
+import {FC} from "react";
+import {useFormStatus} from "react-dom";
+import Button from '@mui/material/Button';
 
-const SubmitButton: FC<{ value: string }> = ({ value }) => {
-	const { pending } = useFormStatus();
-	return (
-		<button
-			type="submit"
-			className={`btn ${styles.buttons}`}
-			disabled={pending}
-		>
-			{pending ? "Loading..." : value}
-		</button>
-	);
+const SubmitButton: FC<{ value: string }> = ({value}) => {
+    const {pending} = useFormStatus();
+    return (
+        <Button
+            type='submit'
+            fullWidth={true}
+            disabled={pending}
+            variant='contained'
+        >
+            {pending ? "Loading..." : value}
+        </Button>
+    );
 };
 
 export default SubmitButton;
