9ba262b7296443ded7ceeff4818ab4ea6870d55b
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:40:14 +0300

    redesign width MUI for Activity component

--- src/pages/ui/Activity/Activity.tsx
+++ src/pages/ui/Activity/Activity.tsx
@@ -1,35 +1,39 @@
 import "@/app/App.css";
 import ActivityPost from "@/widgets/ActivityPost/ui/ActivityPost.tsx";
 import AddPost from "@/features/AddPost/ui/AddPostButton.tsx";
-import { FC, memo } from "react";
-import { PostWithId } from "@/entities/post/model/IPost.ts";
+import {FC, memo} from "react";
+import {Box, Container, List} from '@mui/material';
+import {PostWithId} from "@/entities/post/model/IPost.ts";
 
-const Activity: FC<{ posts: PostWithId[]; checkedTheme: string }> = memo(
-	({ posts, checkedTheme }) => {
-		// TODO: need to show only posts with isPrivate=false
-		// TODO: on Avatar mouseover show a little bit more info about user
-		return (
-			<main className={`wrapper bgColorDefault bgColor${checkedTheme}`}>
-				<AddPost />
-				{posts?.map((post) => {
-					const date = new Date(post.time);
-					return (
-						<ActivityPost
-							userId={post.userId}
-							postText={post.text}
-							key={post.postId}
-							hours={date.getHours().toString().padStart(2, "0")}
-							minutes={date.getMinutes().toString().padStart(2, "0")}
-							day={date.getDate()}
-							month={date.getMonth().toString().padStart(2, "0")}
-							year={date.getFullYear()}
-							checkedTheme={checkedTheme}
-						/>
-					);
-				})}
-			</main>
-		);
-	},
+const Activity: FC<{ posts: PostWithId[] }> = memo(
+    ({posts}) => {
+        // TODO: need to show only posts with isPrivate=false
+        return (
+            <Box component='main' sx={{minHeight: 'calc(100vh - 120px - 10px - 24px)'}}>
+                <Container maxWidth='xl'>
+                    <AddPost/>
+                    <List>
+                        {posts?.map((post, i) => {
+                            const date = new Date(post.time);
+                            return (
+                                <ActivityPost
+                                    userId={post.userId}
+                                    postText={post.text}
+                                    key={post.postId}
+                                    hours={date.getHours().toString().padStart(2, "0")}
+                                    minutes={date.getMinutes().toString().padStart(2, "0")}
+                                    day={date.getDate()}
+                                    month={date.getMonth().toString().padStart(2, "0")}
+                                    year={date.getFullYear()}
+                                    withDivider={i !== posts.length - 1}
+                                />
+                            );
+                        })}
+                    </List>
+                </Container>
+            </Box>
+        );
+    },
 );
 
 export default Activity;
--- src/pages/ui/Activity/ActivityContainer.tsx
+++ src/pages/ui/Activity/ActivityContainer.tsx
@@ -1,12 +1,9 @@
 import Activity from "./Activity.tsx";
-import { useListenPostsQuery } from "@/entities/post/api/post.api.ts";
-import { useAppSelector } from "@/shared/store/lib/reduxHooks.ts";
-import { getCheckedTheme } from "@/shared/store/model/themeSlice.ts";
+import {useListenPostsQuery} from "@/entities/post/api/post.api.ts";
 
 const ActivityContainer = () => {
-	const checkedTheme = useAppSelector(getCheckedTheme);
-	const { data: posts } = useListenPostsQuery();
+    const {data: posts} = useListenPostsQuery();
 
-	return posts && <Activity posts={posts} checkedTheme={checkedTheme} />;
+    return posts && <Activity posts={posts}/>;
 };
 export default ActivityContainer;
