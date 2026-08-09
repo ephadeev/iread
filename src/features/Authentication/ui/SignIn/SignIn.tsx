f1d41a5cfccc9c76b1b802bf2198bd504d63fc65
ephadeev <itrex1991@hotmail.com>
2026-08-09 22:47:15 +0300

    redesign width MUI for SignIn, SignOut and SignUp components

--- src/features/Authentication/ui/SignIn/SignIn.tsx
+++ src/features/Authentication/ui/SignIn/SignIn.tsx
@@ -1,55 +1,94 @@
 import "@/app/App.css";
-import styles from "./signIn.module.css";
-import { FC } from "react";
-import { useAppSelector } from "@/shared/store/lib/reduxHooks.ts";
-import { getCheckedTheme } from "@/shared/store/model/themeSlice.ts";
-import { useSignInMutation } from "@/entities/user/api/auth.api.ts";
+import {FC} from "react";
+import {useSignInMutation} from "@/entities/user/api/auth.api.ts";
 import SubmitButton from "@/shared/ui/SubmitButton/SubmitButton.tsx";
+import {
+    Box,
+    Card,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Stack,
+    TextField,
+    Typography
+} from '@mui/material';
 
 const SignIn: FC = () => {
-	const checkedTheme = useAppSelector(getCheckedTheme);
-	const [signIn] = useSignInMutation();
+    const [signIn] = useSignInMutation();
 
-	const signInHandler = async (formData: FormData) => {
-		const email = formData.get("email");
-		const password = formData.get("password");
-		if (typeof email !== "string" || typeof password !== "string") return;
-		try {
-			await signIn({ email, password }).unwrap();
-		} catch (error) {
-			console.error("Sign In error:", error);
-		}
-	};
+    const signInHandler = async (formData: FormData) => {
+        const email = formData.get("email");
+        const password = formData.get("password");
+        if (typeof email !== "string" || typeof password !== "string") return;
+        try {
+            await signIn({email, password}).unwrap();
+        } catch (error) {
+            console.error("Sign In error:", error);
+        }
+    };
 
-	return (
-		<form action={signInHandler}>
-			<fieldset>
-				<legend>Sign in</legend>
-				<label className={styles.label}>
-					<span>E-mail: </span>
-					<input
-						id="email"
-						type="email"
-						name="email"
-						autoComplete="email"
-						className={`br5 btDefault bt${checkedTheme} ${styles.btn}`}
-					/>
-				</label>
-				<label className={styles.label}>
-					<span>Password: </span>
-					<input
-						id="password"
-						type="password"
-						name="password"
-						minLength={6}
-						autoComplete="current-password"
-						className={`br5 btDefault bt${checkedTheme} ${styles.btn}`}
-					/>
-				</label>
-			</fieldset>
-			<SubmitButton value="Sign in" />
-		</form>
-	);
+    return (
+        <Stack direction='column' sx={{justifyContent: 'space-between'}}>
+            <Card variant='outlined'>
+                <Typography
+                    component='h1'
+                    variant='h4'
+                    sx={{width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)'}}
+                >
+                    Sign in
+                </Typography>
+                <Box
+                    component='form'
+                    action={signInHandler}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor='email'>Email</FormLabel>
+                        <TextField
+                            id='email'
+                            type='email'
+                            name='email'
+                            placeholder='your@email.com'
+                            autoComplete='email'
+                            autoFocus={true}
+                            required={true}
+                            fullWidth={true}
+                            variant='outlined'
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor='password'>Password</FormLabel>
+                        <TextField
+                            id='password'
+                            type='password'
+                            name='password'
+                            placeholder='••••••'
+                            autoComplete='current-password'
+                            autoFocus={true}
+                            required={true}
+                            fullWidth={true}
+                            variant='outlined'
+                        />
+                    </FormControl>
+                    <FormControlLabel
+                        control={<Checkbox value='remember' color='primary'/>}
+                        label='Remember me'
+                    />
+                    <SubmitButton value="Sign in"/>
+                    {/*TODO: implement handler*/}
+                    Forgot your password?
+                    {/*TODO: link to Sign up*/}
+                    Don't have an account? Sign up
+                </Box>
+            </Card>
+        </Stack>
+    );
 };
 
 export default SignIn;
--- src/features/Authentication/ui/SignOut/SignOut.tsx
+++ src/features/Authentication/ui/SignOut/SignOut.tsx
@@ -1,17 +1,12 @@
-import { FC, memo, MouseEvent } from "react";
-import styles from "./signOut.module.css";
+import {FC, memo, MouseEvent} from "react";
+import {Typography} from "@mui/material";
 
 const SignOut: FC<{ signOutHandler: (event: MouseEvent) => void }> = memo(
-	({ signOutHandler }) => {
-		return (
-			<button
-				onClick={signOutHandler}
-				className={`${styles.header__logout} btn`}
-			>
-				Logout
-			</button>
-		);
-	},
+    ({signOutHandler}) => {
+        return (
+            <Typography onClick={signOutHandler}>Logout</Typography>
+        );
+    },
 );
 
 export default SignOut;
